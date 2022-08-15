cf8017800e82986d6cf2ec0cc6417bfb717fc623
Gabriel Montes <gabymontesame@gmail.com>
2022-08-15 12:12:05 -0500

    Configuración del proyecto

---
+++ proyecto_usuarios/proyecto_usuarios/settings/local.py
@@ -0,0 +1,31 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('DB_USER'),
+        'PASSWORD': get_secret('DB_PASSWORD'),
+        'HOST': get_secret('DB_HOST'),
+        'PORT': get_secret('DB_PORT')
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Ruta directorios de archivos estáticos
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+# Ruta directorios de archivos multimedia
+MEDIA_ROOT = BASE_DIR.child('media')
